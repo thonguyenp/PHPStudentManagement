f8d8332f41e4ec065e95affdbbd237ad31031a7d
thonguyenp <nphuoctho2406@gmail.com>
2025-08-01 11:23:56 +0700

    Hoàn thành phần xem student

---
+++ view_student.php
@@ -0,0 +1,97 @@
+<?php
+    session_start();
+    // Nếu cố vào home khi chưa login => trả về login.php
+    if (!isset($_SESSION["username"]) or $_SESSION["usertype"] != "admin")
+    {
+        header("location: login.php");
+    }
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "schoolproject";
+    
+    $data = mysqli_connect($host, $user, $password, $db);
+
+    $sql = "select * from user where usertype = 'student'";
+    $result = mysqli_query($data,$sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Data</title>
+    <?php
+        include "admin_css.php";
+    ?>
+    <style>
+        th {
+            padding: 20px;
+            font-size: 15px;
+            border: 2px solid black;
+        }
+        td {
+            padding: 20px;
+            font-size: 15px;
+            border: 2px solid black;
+            background-color: skyblue;
+        }
+    </style>
+
+</head>
+<body>
+
+    <!-- Sidebar section -->
+    <?php
+        include "admin_sidebar.php";
+    ?>
+    <!-- Sidebar section end -->
+    <!-- Main Content -->
+    <div class="content">
+        <center>
+            <h1>Student Data</h1>
+            <table>
+                <tr>
+                    <th>UserName</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Password</th>
+                </tr>
+                <?php
+                    while ($info = $result -> fetch_assoc())
+                    {
+
+                    
+                ?>
+                    <tr>
+                        <td>
+                            <?php
+                                echo "{$info["username"]}";
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo "{$info["email"]}";
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo "{$info["phone"]}";
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo "{$info["password"]}";
+                            ?>
+                        </td>
+                    </tr>
+
+                <?php
+                    }
+                ?>
+            </table>
+        </center>
+    </div>     
+    <!-- Main Content -->
+</body>
+</html>

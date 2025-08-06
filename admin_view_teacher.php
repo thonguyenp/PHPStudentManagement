d0ded0435c3ae79575b24fcf5dd61c3480a292ad
thonguyenp <nphuoctho2406@gmail.com>
2025-08-06 09:39:16 +0700

    View Teacher bên admin

--- admin_view_teacher.php
+++ admin_view_teacher.php
@@ -1,34 +1,94 @@
 <?php
-    session_start();
-    // Nếu cố vào home khi chưa login => trả về login.php
-    if (!isset($_SESSION["username"]) or $_SESSION["usertype"] != "admin")
-    {
-        header("location: login.php");
-    }
+session_start();
+// Nếu cố vào home khi chưa login => trả về login.php
+if (!isset($_SESSION["username"]) or $_SESSION["usertype"] != "admin") {
+    header("location: login.php");
+}
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "schoolproject";
+
+$data = mysqli_connect($host, $user, $password, $db);
+
+$sql = "select * from teacher";
+$result = mysqli_query($data, $sql);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <?php
-        include "admin_css.php";
+    include "admin_css.php";
     ?>
-    
+    <style>
+        th {
+            padding: 20px;
+            font-size: 15px;
+            border: 2px solid black;
+        }
+
+        td {
+            padding: 20px;
+            font-size: 15px;
+            border: 2px solid black;
+            background-color: skyblue;
+        }
+    </style>
+
 </head>
+
 <body>
 
     <!-- Sidebar section -->
     <?php
-        include "admin_sidebar.php";
+    include "admin_sidebar.php";
     ?>
     <!-- Sidebar section end -->
     <!-- Main Content -->
     <div class="content">
-        <h1>Admin Dashboard</h1>
+        <center>
+            <h1>View Teacher</h1>
+            <table border="1px">
+                <tr>
+                    <th>Teacher Name</th>
+                    <th>About Teacher</th>
+                    <th>Teacher Image</th>
 
-    </div>     
+                </tr>
+                <?php
+                while ($info = $result->fetch_assoc()) {
+
+                ?>
+                    <tr>
+                        <td>
+                            <?php
+                            echo "{$info["name"]}";
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                            echo "{$info["description"]}";
+                            ?>
+                        </td>
+                        <td>
+                            <img height="100px" width="100px" src="<?php
+                            echo "{$info["image"]}";
+                            ?>" alt="">
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </table>
+        </center>
+    </div>
     <!-- Main Content -->
 </body>
+
 </html>
